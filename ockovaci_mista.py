9b7edbadfc3c6fc8baf4aed19d832c34ff1cffe6
Michal Skop <michal.skop@kohovolit.eu>
2021-03-15 11:39:14 +0100

    split ockovaci-mista

---
+++ ockovaci_mista.py
@@ -0,0 +1,12 @@
+"""Split ockovaci mista."""
+
+import pandas as pd
+
+url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovaci-mista.csv"
+
+df = pd.read_csv(url)
+
+months = df['datum'].str[:7].unique()
+
+for month in months:
+    df[df['datum'].str[:7] == month].to_csv("ockovaci-mista_" + month + "_v2.csv")
